a254bfa95367d2bafdaf4be8c23452196e31bbc2
merlinmary <merlilnmary@gmail.com>
2018-08-29 22:54:07 +0530

    adding sample auth file

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google = {
+    clientID: "<YOUR-CLIENT-ID>",
+    clientSecret: "<YOUR-CLIENT-SECRET>",
+    callbackURL: "<YOUR-CALLBACK-URL-eg:http://localhost:1338/auth/google/callback>"
+  }
+}
